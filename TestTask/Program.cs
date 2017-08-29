3491d146e078b3a6e556cdf3636751427b96c8d3
Anstsiya <anastasiya.yenina@gmail.com>
2017-08-29 12:44:41 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           WorkWithFile();
+            
+        }
+        static void WorkWithFile()
+        {
+            try
+            {
+                string path = @"C:\Users\Анастасия Енина\Documents\Visual Studio 2017\Projects\TestTask\File\TextFile.txt";
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                   
+                  // string str = sr.ReadToEnd(). Replace(",", "").Replace(".", "").Replace(":", "").Replace(";", "").Replace("—","").Replace("\n","");
+                    string str = sr.ReadToEnd().Replace("\n","").Replace("\r","");
+               
+                    char[] separators = { ',', '.', ':', ';', ' ', '-', '?' ,'!'};
+                    List<string> words = new List<string>(str.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+                    
+                    Dictionary<string, int> KeyValue = new Dictionary<string, int>();
+
+                    foreach (var v in words)
+                    {
+                        
+                        if(!KeyValue.ContainsKey(v))
+                        {
+                            KeyValue[v] = 1;
+                        }
+                        else
+                        {
+                            KeyValue[v]++;
+                        }
+                    }
+                    foreach (var res in KeyValue)
+                    {
+                        Console.WriteLine(res.Key+" "+res.Value);
+                    }
+                }
+
+            }
+            
+            catch
+            {
+
+            }
+           
+        }
+    }
+}
